6a2b91fc581c28943190fcb3632269db21a293ec
Nosirjonov Qosimjon <nosirjonov.dev@gmail.com>
2025-10-13 23:27:56 +0500

    Splash Activity

--- app/src/main/java/com/example/codialapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/codialapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.codialapp
+package com.example.codialapp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.codialapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
